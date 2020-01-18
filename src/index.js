afc61c5f5b06e1cee562eb5360135e8ca9c16469
koyo-miyamura <nexus9071@gmail.com>
2020-01-19 03:30:56 +0900

    ディレクトリ分けた

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Game from './game.js'
+import Game from './component/game.js'
 import './index.css';
 
 ReactDOM.render(<Game />, document.getElementById("root"));
